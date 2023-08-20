38bb8d6919927ade117348db4977d4d03ec9de4e
Omar Nabil <onabil1998@gmail.com>
2023-08-20 06:30:30 +0300

    Added route53 configuration

--- Terraform/route53.tf
+++ Terraform/route53.tf
@@ -6,7 +6,6 @@ resource "aws_route53_record" "cloudfront_record" {
   zone_id = data.aws_route53_zone.resume.zone_id
   name    = data.aws_route53_zone.resume.name
   type    = "A"
-  ttl     = 300
 
   alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
