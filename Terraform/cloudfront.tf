4bc50c466c3ddec0eb9bd430fb0b74ee6e891b3f
Omar Nabil <onabil1998@gmail.com>
2023-08-20 06:36:09 +0300

    Added route53 configuration

--- Terraform/cloudfront.tf
+++ Terraform/cloudfront.tf
@@ -27,7 +27,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1"
+
   }
 
   depends_on = [
